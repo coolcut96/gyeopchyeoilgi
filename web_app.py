9d5842fd9b9e16fd35c66f4c1d0b14db8c5cc161
coolcut96 <coolcut96@gmail.com>
2026-09-09 18:24:17 +0900

    Add files via upload

--- web_app.py
+++ web_app.py
@@ -57,9 +57,9 @@ BOOKS = [
     {"title": "별을 보다가 사주를 펼쳤다", "brtitle": "별을 보다가<br>사주를 펼쳤다", "sub": "사주인을 위한 점성술 입문", "cls": "c1", "emoji": "📘",
      "stores": {"예스24": "https://www.yes24.com/product/goods/194750574", "알라딘": "", "리디북스": "https://ridibooks.com/books/805051233", "교보문고": ""}},
     {"title": "여덟 글자 뒤의 별들", "brtitle": "여덟 글자<br>뒤의 별들", "sub": "점성술 렌즈로 본 사주의 구조", "cls": "c2", "emoji": "📗",
-     "stores": {"예스24": "https://www.yes24.com/product/goods/195237163", "알라딘": "https://www.aladin.co.kr/shop/wproduct.aspx?ItemId=400603015", "리디북스": "", "교보문고": ""}},
+     "stores": {"예스24": "https://www.yes24.com/product/goods/195237163", "알라딘": "https://www.aladin.co.kr/shop/wproduct.aspx?ItemId=400603015", "리디북스": "https://ridibooks.com/search?q=%EC%97%AC%EB%8D%9F+%EA%B8%80%EC%9E%90+%EB%92%A4%EC%9D%98+%EB%B3%84%EB%93%A4&adult_exclude=n", "교보문고": ""}},
     {"title": "일단 해보자 점성술", "brtitle": "일단 해보자<br>점성술", "sub": "직접 따라 하며 익히는 실전 점성술", "cls": "c3", "emoji": "📙",
-     "stores": {"예스24": "https://www.yes24.com/product/goods/195237164", "알라딘": "https://www.aladin.co.kr/shop/wproduct.aspx?ItemId=400603251", "리디북스": "", "교보문고": ""}},
+     "stores": {"예스24": "https://www.yes24.com/product/goods/195237164", "알라딘": "https://www.aladin.co.kr/shop/wproduct.aspx?ItemId=400603251", "리디북스": "https://ridibooks.com/books/805051312", "교보문고": ""}},
 ]
 PUBLISHER = "별읽기 · Star Reading"
 
